3fca9f35b82746951a6dcf2009e51649b1b1f051
DragonFury24 <dennisdnguyen19@gmail.com>
2017-09-29 11:13:52 -0400

    .

--- src/LinesMain.java
+++ src/LinesMain.java
@@ -4,7 +4,8 @@ public class LinesMain {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         double x1, y1, x2, y2;
-        LineSegment lineSegment;
+        LineSegment lineSegment1;
+        LineSegment[] lineSegments;
         System.out.println("Input first x value");
         x1 = keyboard.nextDouble();
         System.out.println("Input first y value");
@@ -14,12 +15,12 @@ public class LinesMain {
         System.out.println("Input second y value");
         y2 = keyboard.nextDouble();
         keyboard.nextLine();
-        lineSegment = new LineSegment(x1, y1, x2, y2);
-        System.out.println("Points: " + lineSegment.toString());
-        System.out.println("Length: " + lineSegment.getLength());
-        System.out.println("Slope: " + lineSegment.getSlope());
-        System.out.println("Intersects x-axis: " + lineSegment.intersectsX());
-        System.out.println("Intersects y-axis: " + lineSegment.intersectsY());
+        lineSegments[0] = new LineSegment(x1, y1, x2, y2);
+        System.out.println("Points: " + lineSegments[0].toString());
+        System.out.println("Length: " + lineSegments[0].getLength());
+        System.out.println("Slope: " + lineSegments[0].getSlope());
+        System.out.println("Intersects x-axis: " + lineSegments[0].intersectsX());
+        System.out.println("Intersects y-axis: " + lineSegments[0].intersectsY());
         Line.LINE_SEGMENT.intersectsY();
     }
     public static class Line {
