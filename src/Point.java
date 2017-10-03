cacb61f9c0b84c475e0cb23af64b86280d7a9136
DragonFury24 <dennisdnguyen19@gmail.com>
2017-10-03 08:46:04 -0400

    Add comments to Point

--- src/Point.java
+++ src/Point.java
@@ -6,13 +6,17 @@ public class Point {
 
     private double xCoordinate, yCoordinate;
 
+    //return xCoordinate value
     public double getX() {
         return xCoordinate;
     }
 
+    //return yCoordinate value
     public double getY() {
         return yCoordinate;
     }
+
+    //returns both xCoordinate and yCoordinate as String
     public String toString() {
         return "(" + xCoordinate + ", " + yCoordinate + ")";
     }
